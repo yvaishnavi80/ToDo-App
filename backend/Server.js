40374ed7889d16182b81ea53269d42a672cf55d2
yvaishnavi80 <145574044+yvaishnavi80@users.noreply.github.com>
2023-11-12 16:10:53 +0530

    Add files via upload

---
+++ backend/Server.js
@@ -0,0 +1,16 @@
+//https://www.youtube.com/watch?v=PvMDWbAPPK4
+const express = require('express')
+const mongoose= require('mongoose')
+const cors=require("cors")
+const routes=require('./routes/ToDoRoute')
+require('dotenv').config()
+const app=express()
+const PORT=process.env.PORT||5000
+app.use(express.json())
+app.use(cors())
+mongoose
+.connect(process.env.MONGODB_URL)
+.then(()=>console.log(`Connected to MongoDb...`))
+.catch((err)=>console.log(err))
+app.use(routes)
+app.listen(PORT,()=>console.log(`Listening on: ${PORT}`))
